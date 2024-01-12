6d54b1f3f16e0d3ea27e2354402bd8fce3516cf2
morgan <mkywall3@gmail.com>
2024-01-11 17:08:43 -0800

    search user by email and logout

--- mf_crucible_controlpanel.py
+++ mf_crucible_controlpanel.py
@@ -1,8 +1,10 @@
 from ScopeFoundry.measurement import Measurement
+from qtpy.QtWidgets import QPushButton, QLabel
 
 class MFCrucibleControlPanel(Measurement):
     
-    name = "mf_crucible_control"
+    name = "user_login_mf_crucible"
+    
     
     def setup(self):
         #self.user_info = self.app.hardware['mf_crucible']
@@ -10,3 +12,19 @@ class MFCrucibleControlPanel(Measurement):
     
     def setup_figure(self):
         self.ui = self.app.hardware['mf_crucible'].settings.New_UI()
+        self.hw_name = "mf_crucible"
+        self.mf_crucible = self.app.hardware[self.hw_name]
+        
+        logout_push_button = QPushButton('Log Out')
+        self.ui.layout().addWidget(logout_push_button)
+        logout_push_button.clicked.connect(self.clear_userinfo)
+
+    def clear_userinfo(self):
+        print("ya clicked")
+        print(self.mf_crucible.settings.proposal)
+        self.mf_crucible.settings['user_name'] = ""
+        self.mf_crucible.settings['orcid'] = "XXXX-XXXX-XXXX-XXXX"
+        self.mf_crucible.settings['email'] = "user-email@lbl.gov"
+        prop_lq = self.mf_crucible.settings.get_lq('proposal')
+        prop_lq.change_choice_list([])
+        prop_lq.update_value("")
--- mf_crucible_hw.py
+++ mf_crucible_hw.py
@@ -7,18 +7,33 @@ class MFCrucibleHW(HardwareComponent):
     name = 'mf_crucible'
     
     def setup(self):
-        
-        self.settings.New("orcid", initial="0000-0000-0000-0000", dtype=str)
-        self.settings.New("proposal", initial="MFP0000", choices=(['MFP0000']), dtype=str)
-        self.settings.New("email", initial="nobody@lbl.gov",dtype=str)
-        self.settings.New("user_name", initial = "nobody", dtype = str)
+        self.settings.New("email", initial="user-email@lbl.gov",dtype=str)
+        self.settings.New("user_name", initial = "", dtype = str)
+        self.settings.New("orcid", initial="XXXX-XXXX-XXXX-XXXX", dtype=str)
+        self.settings.New("proposal", initial="", choices=([]), dtype=str)
+
         
         self.settings.orcid.add_listener(self.on_enter_orcid_id, argtype = str)
-        
+        self.settings.email.add_listener(self.on_enter_email, argtype = str)
         #self.add_operation("User Login", self.login_opfunc)
 
     #def login_opfunc(self):
       #  print("user login operation running")
+    def update_proposals(self, user_info):
+        email = user_info['lbl_email'] if user_info['lbl_email'] is not None else user_info['email']
+        
+        internal_research_val = f"MFUSER_{email.split('@')[0]} (InternalResearch)"
+        proposal_list = user_info['proposals']
+        proposal_list.append(internal_research_val)
+        prop_lq = self.settings.get_lq('proposal')
+        prop_lq.change_choice_list(proposal_list)
+        prop_lq.update_value(internal_research_val)
+
+    def update_username(self, user_info):
+        # update name
+        user_name = f"{user_info['first_name']} {user_info['last_name']}"
+        user_name_lq = self.settings.get_lq('user_name')
+        user_name_lq.update_value(user_name)
         
     def on_enter_orcid_id(self):
         orcid = self.settings.orcid.value.replace("-", "")
@@ -28,24 +43,56 @@ class MFCrucibleHW(HardwareComponent):
             user_info = get_proposals_using_orcid(orcid_format)
             print(user_info)
             if user_info is not None:
-                # update proposal dropdown options
-                proposal_list = user_info['proposals']
-                proposal_list.append("InternalResearch")
-                prop_lq = self.settings.get_lq('proposal')
-                prop_lq.change_choice_list(proposal_list)
-                prop_lq.update_value("InternalResearch")
-
                 # update user email
                 email = user_info['lbl_email'] if user_info['lbl_email'] is not None else user_info['email']
                 email_lq = self.settings.get_lq('email')
                 email_lq.update_value(email)
-                
-                # update name
-                user_name = f"{user_info['first_name']} {user_info['last_name']}"
-                user_name_lq = self.settings.get_lq('user_name')
-                user_name_lq.update_value(user_name)
 
-            
+                # update proposal dropdown options
+                self.update_proposals(user_info)
+                
+                # update user name
+                self.update_username(user_info)
+
+                
+                
+    def on_enter_email(self):
+            email = self.settings.email.value.strip()
+            user_info = get_proposals_using_email(email)
+            print(user_info)
+            if user_info is not None:
+                # proposal dropdown options
+                self.update_proposals(user_info)
+
+                # update name
+                self.update_username(user_info)
+
+                # update orcid
+                orcid = user_info['orcid']
+                if orcid is not None:
+                    orcid_lq = self.settings.get_lq('orcid')
+                    orcid_lq.update_value(orcid)
+                else:
+                    print("user needs to add orcid to their user profile")
+                
+                
+    
+
+
+
+
+def get_proposals_using_email(email):
+    with open("secrets/proposaldb.yaml") as f:
+        secrets= yaml.safe_load(f)
+    f.close()
+    apikey = secrets["apikey"]
+    response = requests.request(method="get", url=f"https://foundry-admin.lbl.gov/api/json/scicat/getuserbyemail.aspx?key={apikey}&email={email}")
+    if response.text != '' and response.status_code == 200:
+        return(response.json())
+    else:
+        return(None)
+
+
 def get_proposals_using_orcid(orcid_id):
     with open("secrets/proposaldb.yaml") as f:
         secrets= yaml.safe_load(f)
